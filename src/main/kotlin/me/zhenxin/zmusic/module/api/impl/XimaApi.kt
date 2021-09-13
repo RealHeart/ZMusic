40f3a0859e15643b96d6330d6d6b1c58f7e6af4c
真心 <ceo@iqianye.cn>
2021-09-13 16:39:29 +0800

    Api: 喜马拉雅实现

--- src/main/kotlin/me/zhenxin/zmusic/module/api/impl/XimaApi.kt
+++ src/main/kotlin/me/zhenxin/zmusic/module/api/impl/XimaApi.kt
@@ -1,8 +1,11 @@
 package me.zhenxin.zmusic.module.api.impl
 
+import cn.hutool.json.JSONObject
 import me.zhenxin.zmusic.module.Lang
 import me.zhenxin.zmusic.module.api.MusicApi
 import me.zhenxin.zmusic.module.api.MusicInfo
+import me.zhenxin.zmusic.utils.HttpUtil
+import java.net.URLEncoder
 
 /**
  * 喜马拉雅实现
@@ -16,7 +19,38 @@ class XimaApi : MusicApi {
     override val name: String = Lang.PLATFORM_XIMA
 
     override fun searchPage(keyword: String, page: Int, count: Int): MutableList<MusicInfo> {
-        TODO("Not yet implemented")
+        val musics = mutableListOf<MusicInfo>()
+        val search =
+            HttpUtil.get(
+                "https://www.ximalaya.com/revision/search/main?kw=${
+                    URLEncoder.encode(keyword, "UTF-8")
+                }&core=track&page=$page&rows=$count"
+            )
+        val data = JSONObject(search.data)
+        val result = data.getJSONObject("data")
+        val track = result.getJSONObject("track")
+        val songs = track.getJSONArray("docs")
+        songs.forEach {
+            it as JSONObject
+
+            val id = it.getStr("id")
+            val name = it.getStr("title")
+            val albumName = it.getStr("albumTitle")
+            val albumImage = it.getStr("albumCoverPath")
+            val duration = it.getLong("duration") * 1000
+
+            musics.add(
+                MusicInfo(
+                    id,
+                    name,
+                    this.name,
+                    albumName,
+                    albumImage,
+                    duration
+                )
+            )
+        }
+        return musics
     }
 
     override fun getPlaylist(id: String) {
@@ -28,6 +62,12 @@ class XimaApi : MusicApi {
     }
 
     override fun getPlayUrl(id: String): String {
-        TODO("Not yet implemented")
+        val result =
+            HttpUtil.get("https://mobile.ximalaya.com/mobile-playpage/playpage/tabs/$id/ts-${System.currentTimeMillis()}")
+        val json = JSONObject(result.data)
+        val data = json.getJSONObject("data")
+        val page = data.getJSONObject("playpage")
+        val track = page.getJSONObject("trackInfo")
+        return track.getStr("playUrl64")
     }
 }
