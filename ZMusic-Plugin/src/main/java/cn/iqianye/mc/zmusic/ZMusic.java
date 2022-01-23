592768d42e251d8c51bef0339da54bb2b5a815fc
WhkSoft <whksoft@gmail.com>
2022-01-23 20:43:04 +0800

    Edit version number

--- ZMusic-Plugin/src/main/java/cn/iqianye/mc/zmusic/ZMusic.java
+++ ZMusic-Plugin/src/main/java/cn/iqianye/mc/zmusic/ZMusic.java
@@ -27,7 +27,7 @@ public final class ZMusic {
 
     public static File dataFolder;
     public static String thisVer;
-    public static int thisVerCode = 202111111;
+    public static int thisVerCode = 202201230;
     public static boolean bilibiliIsVIP = false;
     public static boolean isViaVer = true;
     public static boolean isEnable = true;
