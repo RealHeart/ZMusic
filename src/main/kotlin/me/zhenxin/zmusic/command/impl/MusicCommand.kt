0fe6fc660eb3d3c4d3afe17422dbaac13d1a2311
真心 <ceo@iqianye.cn>
2022-06-17 04:10:15 +0800

    命令: /zm music

--- src/main/kotlin/me/zhenxin/zmusic/command/impl/MusicCommand.kt
+++ src/main/kotlin/me/zhenxin/zmusic/command/impl/MusicCommand.kt
@@ -1,6 +1,18 @@
 package me.zhenxin.zmusic.command.impl
 
+import me.zhenxin.zmusic.config.Lang
+import me.zhenxin.zmusic.enums.asMusicPlatform
+import me.zhenxin.zmusic.enums.getPlatformNames
+import me.zhenxin.zmusic.proto.sendToast
+import me.zhenxin.zmusic.taboolib.extend.sendMsg
+import me.zhenxin.zmusic.utils.asMusicApi
+import me.zhenxin.zmusic.utils.colored
+import me.zhenxin.zmusic.utils.isChina
+import me.zhenxin.zmusic.utils.playMusic
+import taboolib.common.platform.ProxyPlayer
 import taboolib.common.platform.command.subCommand
+import taboolib.common.platform.function.onlinePlayers
+import taboolib.common.platform.function.submit
 
 /**
  * 点歌命令
@@ -10,5 +22,50 @@ import taboolib.common.platform.command.subCommand
  * @email qgzhenxin@qq.com
  */
 val musicCommand = subCommand {
-
+    dynamic(commit = Lang.COMMAND_SUGGESTION_PLATFORM) {
+        suggestion<ProxyPlayer> { _, _ ->
+            getPlatformNames()
+        }
+        dynamic(commit = Lang.COMMAND_SUGGESTION_SONG) {
+            suggestion<ProxyPlayer>(true) { _, _ ->
+                listOf("[${Lang.COMMAND_SUGGESTION_SONG}]")
+            }
+            execute<ProxyPlayer> { sender, context, argument ->
+                sender.sendMsg(Lang.COMMAND_SEARCHING)
+                val platform = context.argument(-1)
+                if (platform == "soundcloud") {
+                    if (isChina()) {
+                        sender.sendMsg(Lang.NO_SUPPORTED_REGION)
+                        return@execute
+                    }
+                }
+                if (argument.contains("-id:")) {
+                    val p = platform.asMusicPlatform()
+                    if (!p.supportIdPlay) {
+                        sender.sendMsg(Lang.COMMAND_NOSUPPORTED_IDPLAY)
+                        return@execute
+                    }
+                }
+                val api = platform.asMusicApi()
+                submit(async = true) {
+                    val result = api.searchSingle(argument)
+                    val url = api.getPlayUrl(result.id)
+                    if (url.isEmpty()) {
+                        return@submit
+                    }
+                    onlinePlayers().forEach {
+                        it.playMusic(url)
+                        it.sendToast(Lang.TOAST_PLAYING.replace("{0}", result.name).colored())
+                        it.sendMsg(
+                            Lang.COMMAND_MUSIC_SUCCESS
+                                .replace("{0}", sender.name)
+                                .replace("{1}", api.name)
+                                .replace("{2}", "${result.singer} - ${result.name}") +
+                                    " <click:run_command:'/zm stop'><hover:show_text:'${Lang.MESSAGE_JSON_TIPS}'>&f[&c${Lang.MESSAGE_JSON_STOP}&f]</click>"
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
