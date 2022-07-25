3521209ae99e1213ae296d4691f512721e294fdb
真心 <ceo@iqianye.cn>
2022-07-25 17:00:23 +0800

    [bridge]: PAPI变量实现

--- zmusic-bridge/src/main/java/me/zhenxin/zmusic/bridge/ZMusicBridge.java
+++ zmusic-bridge/src/main/java/me/zhenxin/zmusic/bridge/ZMusicBridge.java
@@ -4,6 +4,8 @@ import me.zhenxin.zmusic.bridge.api.PlaceholderAPI;
 import me.zhenxin.zmusic.bridge.api.PluginMessage;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Locale;
+
 /**
  * @author 真心
  * @email qgzhenxin@qq.com
@@ -19,14 +21,28 @@ public class ZMusicBridge extends JavaPlugin {
         if (getServer()
                 .getPluginManager()
                 .isPluginEnabled("ZMusic")) {
-            setEnabled(false);
+            Locale locale = Locale.getDefault();
+            if (locale == Locale.SIMPLIFIED_CHINESE
+                    || locale == Locale.TRADITIONAL_CHINESE
+                    || locale == Locale.CHINESE) {
+                getLogger().warning("检测到ZMusic插件已启用，请移除ZMusic插件。");
+                getLogger().warning("ZMusicBridge 用于BungeeCord/Velocity端与子服通信，请勿同时启用ZMusic和ZMusicBridge。");
+            } else {
+                getLogger().warning("Checking ZMusic plugin is enabled, please remove ZMusic plugin.");
+                getLogger().warning("ZMusicBridge is used to communicate between BungeeCord/Velocity server and sub-server, please do not enable ZMusic and ZMusicBridge at the same time.");
+            }
         }
         plugin = this;
         ZMusicBridge.isSupportPlaceholderAPI = getServer()
                 .getPluginManager()
                 .isPluginEnabled("PlaceholderAPI");
         if (isSupportPlaceholderAPI) {
-            new PlaceholderAPI().register();
+            boolean register = new PlaceholderAPI().register();
+            if (!register) {
+                getLogger().warning("PlaceholderAPI register failed!");
+            }
+        } else {
+            getLogger().warning("PlaceholderAPI is not enabled.");
         }
         getServer()
                 .getMessenger()
--- zmusic-bridge/src/main/java/me/zhenxin/zmusic/bridge/api/PlaceholderAPI.java
+++ zmusic-bridge/src/main/java/me/zhenxin/zmusic/bridge/api/PlaceholderAPI.java
@@ -1,10 +1,11 @@
 package me.zhenxin.zmusic.bridge.api;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.zhenxin.zmusic.bridge.data.PlayerData;
 import org.bukkit.entity.Player;
 
 
-@SuppressWarnings({"unused", "AlibabaClassMustHaveAuthor", "AlibabaClassNamingShouldBeCamel", "NullableProblems"})
+@SuppressWarnings({"unused", "AlibabaClassMustHaveAuthor", "AlibabaClassNamingShouldBeCamel", "NullableProblems", "AlibabaUndefineMagicConstant"})
 public class PlaceholderAPI extends PlaceholderExpansion {
 
     @Override
@@ -24,7 +25,55 @@ public class PlaceholderAPI extends PlaceholderExpansion {
 
     @Override
     public String onPlaceholderRequest(Player player, String params) {
-        // TODO: 暂未实现
-        return null;
+        switch (params) {
+            case "status_name":
+                String name = PlayerData.getName(player);
+                if (name != null) {
+                    return name;
+                } else {
+                    return "None";
+                }
+            case "status_singer":
+                String singer = PlayerData.getSinger(player);
+                if (singer != null) {
+                    return singer;
+                } else {
+                    return "None";
+                }
+            case "status_lyric":
+                String lyric = PlayerData.getLyric(player);
+                if (lyric != null) {
+                    return lyric;
+                } else {
+                    return "None";
+                }
+            case "status_current_time":
+                Long currentTime = PlayerData.getCurrentTime(player);
+                return formatTime(currentTime);
+            case "status_max_time":
+                Long maxTime = PlayerData.getMaxTime(player);
+                return formatTime(maxTime);
+            default:
+                return null;
+        }
+    }
+
+    private String formatTime(Long time) {
+        if (time != null) {
+            if (time < 60) {
+                return "00" + ":" + String.format("%02d", time);
+            } else if (time < 3600) {
+                long m = time / 60;
+                long s = time % 60;
+                return String.format("%02d", m) + ":" + String.format("%02d", s);
+            } else {
+                long h = time / 3600;
+                long m = (time % 3600) / 60;
+                long s = (time % 3600) % 60;
+                return String.format("%02d", h) + ":" + String.format("%02d", m) + ":" + String.format("%02d", s);
+            }
+        } else {
+            return "--:--";
+        }
     }
 }
