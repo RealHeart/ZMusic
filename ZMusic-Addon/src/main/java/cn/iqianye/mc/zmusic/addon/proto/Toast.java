e9bf345c9f8e276f6b7528cc0c70394045a381d4
真心 <ceo@iqianye.cn>
2022-12-20 18:06:31 +0800

    Support 1.19.3 (NMS 1.19_R2)

--- ZMusic-Addon/src/main/java/cn/iqianye/mc/zmusic/addon/proto/Toast.java
+++ ZMusic-Addon/src/main/java/cn/iqianye/mc/zmusic/addon/proto/Toast.java
@@ -20,6 +20,9 @@ public class Toast {
         Player bukkitPlayer = (Player) player;
         AdvancementPacket packet;
         switch (nms) {
+            case "v1_19_R2":
+                packet = new AdvancementPacket_1_19_R2(bukkitPlayer, title);
+                break;
             case "v1_19_R1":
                 packet = new AdvancementPacket_1_19_R1(bukkitPlayer, title);
                 break;
