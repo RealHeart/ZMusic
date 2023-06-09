cc49002ade74bd14f26603c23ae6bf54a81c8dfd
真心 <qgzhenxin@qq.com>
2023-06-09 20:55:24 +0800

    chore: Bridge 3.2.0 Released

--- zmusic-bridge/build.gradle.kts
+++ zmusic-bridge/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-version = "3.1.0"
+version = "3.2.0"
 
 dependencies {
     implementation(project(":zmusic-nms:zmusic-nms-core"))
