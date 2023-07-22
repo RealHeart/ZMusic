fe24d76d1106531b7120c93a20847ed567127be5
真心 <qgzhenxin@qq.com>
2023-07-22 09:03:48 +0800

    fix: 部分服务端无法使用 Toast

--- ZMusic-Plugin/src/main/java/me/zhenxin/zmusic/proto/Toast.java
+++ ZMusic-Plugin/src/main/java/me/zhenxin/zmusic/proto/Toast.java
@@ -15,59 +15,63 @@ import org.bukkit.entity.Player;
 public class Toast {
 
     public static void sendToast(Object player, String title) {
-        String packageName = ZMusicBukkit.plugin.getServer().getClass().getPackage().getName();
-        String nms = packageName.substring(packageName.lastIndexOf('.') + 1);
-        Player bukkitPlayer = (Player) player;
-        AdvancementPacket packet;
-        switch (nms) {
-            case "v1_20_R1":
-                packet = new AdvancementPacket_1_20_R1(bukkitPlayer, title);
-                break;
-            case "v1_19_R3":
-                packet = new AdvancementPacket_1_19_R3(bukkitPlayer, title);
-                break;
-            case "v1_19_R2":
-                packet = new AdvancementPacket_1_19_R2(bukkitPlayer, title);
-                break;
-            case "v1_19_R1":
-                packet = new AdvancementPacket_1_19_R1(bukkitPlayer, title);
-                break;
-            case "v1_18_R2":
-                packet = new AdvancementPacket_1_18_R2(bukkitPlayer, title);
-                break;
-            case "v1_18_R1":
-                packet = new AdvancementPacket_1_18_R1(bukkitPlayer, title);
-                break;
-            case "v1_17_R1":
-                packet = new AdvancementPacket_1_17_R1(bukkitPlayer, title);
-                break;
-            case "v1_16_R3":
-                packet = new AdvancementPacket_1_16_R3(bukkitPlayer, title);
-                break;
-            case "v1_16_R2":
-                packet = new AdvancementPacket_1_16_R2(bukkitPlayer, title);
-                break;
-            case "v1_16_R1":
-                packet = new AdvancementPacket_1_16_R1(bukkitPlayer, title);
-                break;
-            case "v1_15_R1":
-                packet = new AdvancementPacket_1_15_R1(bukkitPlayer, title);
-                break;
-            case "v1_14_R1":
-                packet = new AdvancementPacket_1_14_R1(bukkitPlayer, title);
-                break;
-            case "v1_13_R2":
-                packet = new AdvancementPacket_1_13_R2(bukkitPlayer, title);
-                break;
-            case "v1_12_R1":
-                packet = new AdvancementPacket_1_12_R1(bukkitPlayer, title);
-                break;
-            default:
-                ZMusic.log.sendErrorMessage("不支持的NMS版本: " + nms);
-                return;
-        }
+        try {
+            String packageName = ZMusicBukkit.plugin.getServer().getClass().getPackage().getName();
+            String nms = packageName.substring(packageName.lastIndexOf('.') + 1);
+            Player bukkitPlayer = (Player) player;
+            AdvancementPacket packet;
+            switch (nms) {
+                case "v1_20_R1":
+                    packet = new AdvancementPacket_1_20_R1(bukkitPlayer, title);
+                    break;
+                case "v1_19_R3":
+                    packet = new AdvancementPacket_1_19_R3(bukkitPlayer, title);
+                    break;
+                case "v1_19_R2":
+                    packet = new AdvancementPacket_1_19_R2(bukkitPlayer, title);
+                    break;
+                case "v1_19_R1":
+                    packet = new AdvancementPacket_1_19_R1(bukkitPlayer, title);
+                    break;
+                case "v1_18_R2":
+                    packet = new AdvancementPacket_1_18_R2(bukkitPlayer, title);
+                    break;
+                case "v1_18_R1":
+                    packet = new AdvancementPacket_1_18_R1(bukkitPlayer, title);
+                    break;
+                case "v1_17_R1":
+                    packet = new AdvancementPacket_1_17_R1(bukkitPlayer, title);
+                    break;
+                case "v1_16_R3":
+                    packet = new AdvancementPacket_1_16_R3(bukkitPlayer, title);
+                    break;
+                case "v1_16_R2":
+                    packet = new AdvancementPacket_1_16_R2(bukkitPlayer, title);
+                    break;
+                case "v1_16_R1":
+                    packet = new AdvancementPacket_1_16_R1(bukkitPlayer, title);
+                    break;
+                case "v1_15_R1":
+                    packet = new AdvancementPacket_1_15_R1(bukkitPlayer, title);
+                    break;
+                case "v1_14_R1":
+                    packet = new AdvancementPacket_1_14_R1(bukkitPlayer, title);
+                    break;
+                case "v1_13_R2":
+                    packet = new AdvancementPacket_1_13_R2(bukkitPlayer, title);
+                    break;
+                case "v1_12_R1":
+                    packet = new AdvancementPacket_1_12_R1(bukkitPlayer, title);
+                    break;
+                default:
+                    ZMusic.log.sendErrorMessage("不支持的NMS版本: " + nms);
+                    return;
+            }
 
-        packet.grant();
-        packet.revoke();
+            packet.grant();
+            packet.revoke();
+        } catch (Exception e) {
+            ZMusic.log.sendDebugMessage("当前服务端不支持Toast功能.");
+        }
     }
 }
