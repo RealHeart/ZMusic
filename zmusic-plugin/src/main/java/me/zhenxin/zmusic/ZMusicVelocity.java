fc19ad700920c42bac3165afb1a19a7de3a90502
真心 <qgzhenxin@qq.com>
2026-07-24 13:54:38 +0800

    chore: 升级版本至 2.13.5-beta

--- zmusic-plugin/src/main/java/me/zhenxin/zmusic/ZMusicVelocity.java
+++ zmusic-plugin/src/main/java/me/zhenxin/zmusic/ZMusicVelocity.java
@@ -27,7 +27,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "zmusic",
         name = "ZMusic",
-        version = "2.13.0",
+        version = "2.13.5-beta",
         description = "A Minecraft music plugin supporting Bukkit, BungeeCord, and Velocity",
         url = "https://github.com/zhenxin/ZMusic",
         authors = {"ZhenXin"}
@@ -70,7 +70,7 @@ public class ZMusicVelocity {
         Config.realSupportBossBar = false;
         Config.realSupportVault = false;
         Config.debug = true;
-        ZMusic.thisVer = "2.13.0";
+        ZMusic.thisVer = "2.13.5-beta";
         ZMusic.log.sendNormalMessage("正在加载中....");
         CookieUtils.initCookieManager();
 
